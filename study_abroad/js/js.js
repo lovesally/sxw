7b9ef744d05aa103d4bb91d29079f536d4fd93f1
lovesally <997673404@qq.com>
2014-10-18 09:07:05 +0800

    申晓唯
    申晓唯游学作业

---
+++ study_abroad/js/js.js
@@ -0,0 +1,58 @@
+;(function(window,$){
+    $(function(){
+        var $head_ul=$('#head_list'),
+            $head_li=$head_ul.children('li'),
+            len=$head_li.length;
+        var $news_li=$('.news_list').find('li');
+        /*点击头部菜单列表显示下拉菜单*/
+        $head_li.on('click',function(){
+            if($('#head_list>li:has(ul)')){
+                $(this).find('.head_child_list').show().parent('li').siblings().find('.head_child_list').hide();
+
+            }
+        });
+        /*鼠标经过导航栏的效果*/
+        $('.menu>li').on('hover',function(){
+            var ind=$(this).index()-1;
+            $(this).toggleClass('cur');
+            if( $('.country_list>ul').eq(ind)){
+                $('.country_list>ul').eq(ind).toggleClass('on');
+            }
+        });
+        /*鼠标经过侧边栏的效果*/
+        $('.main_one_left li').on('hover',function(){
+            $(this).toggleClass('current');
+            if($(this).children('div').hasClass('second_menu')){
+                $(this).find('.second_menu').toggle();
+            }
+        });
+        /*最新资讯等菜单的效果*/
+        $('.news_nav li').eq(0).addClass('hovers');
+        $('.news_nav li').on('hover',function(){
+            var index=$(this).index();
+            $(this).addClass('hovers').siblings().removeClass('hovers');
+            $('.news_list>ul').eq(index).show().siblings().hide();
+        });
+        $('.news_list ul').eq(0).show();
+        $news_li.on('hover',function(){
+            $(this).addClass('new_cur').siblings().removeClass('new_cur');
+        });
+        var text = $('.search_text').val();
+        $('.search_text').on('focus',function(){
+            var onText=$(this).val();
+            if(text==onText){
+                $(this).val('');
+            }
+        });
+        $('.search_text').on('blur',function(){
+            if($(this).val()==''){
+                $(this).val(text);
+            }
+        });
+        $('.pic_nav>li').on('hover',function(){
+            var index=$(this).index();
+            $('.pics>img').hide().eq(index).fadeIn(500);
+            $(this).addClass('onl').siblings().removeClass();
+        })
+    })
+})(window,jQuery);
